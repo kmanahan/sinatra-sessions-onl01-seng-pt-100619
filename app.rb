3a52b281ef3b1de0d946aa53ce85e38684927655
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-30 15:23:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
     @session = session
-    session[:foo] = params[:foo]
+    session[:foo] = params[:hello]
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
